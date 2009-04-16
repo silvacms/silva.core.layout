fdeadab386d5169c4ec1bc443308e19dad6438a6
Sylvain Viollon <thefunny@gmail.com>
2009-04-16 15:14:54 +0000

    the core layout is installable.

--- silva/core/layout/__init__.py
+++ silva/core/layout/__init__.py
@@ -3,3 +3,22 @@
 # See also LICENSE.txt
 # $Id$
 
+from Products.Silva.ExtensionRegistry import extensionRegistry
+from silva.core.conf.installer import DefaultInstaller
+import zope.interface
+
+
+class IExtension(zope.interface.Interface):
+    """silva.core.layout extension.
+    """
+
+class CoreLayoutInstaller(DefaultInstaller):
+    __name__ = "silva.core.layout.install"
+
+install = CoreLayoutInstaller('Silva Core Layout', IExtension)
+
+
+def initialize(context):
+    extensionRegistry.register(
+        'Silva Core Layout', 'Silva 2.2 Layout system compatiblity',
+        context, [], install, depends_on=('Silva', 'SilvaLayout',),)
