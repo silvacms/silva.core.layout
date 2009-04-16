e0bdc083ff1928a537374ec3fbb384153d7f5301
Sylvain Viollon <thefunny@gmail.com>
2009-04-16 15:30:17 +0000

    Add some stuff to the installer.

--- silva/core/layout/__init__.py
+++ silva/core/layout/__init__.py
@@ -15,6 +15,22 @@ class IExtension(zope.interface.Interface):
 class CoreLayoutInstaller(DefaultInstaller):
     __name__ = "silva.core.layout.install"
 
+
+    def install_custom(self, root):
+        if hasattr(root, 'preview_html'):
+            root.manage_delObjects(['preview_html',])
+        root.manage_delObjects(['standard_error_message',])
+        factory = root.manage_addProduct['PythonScripts']
+        factory.manage_addPythonScript('standard_error_message')
+        sem = getattr(root, 'standard_error_message')
+        sem.write("""##parameters=error_type,error_value,error_traceback,error_tb,error_message,error_log_url
+from silva.core.layout.compat import standard_error_message
+
+return standard_error_message(context, context.REQUEST, error_value)
+""")
+
+
+
 install = CoreLayoutInstaller('Silva Core Layout', IExtension)
 
 
