2d70dae4ff568f2a18f157e31147847efb72b2e0
Sylvain Viollon <thefunny@gmail.com>
2009-11-12 09:29:33 +0000

    Make the fallback layout used for SMI like components more generic.

--- silva/core/layout/porto/z3attributefix.py
+++ silva/core/layout/porto/z3attributefix.py
@@ -1,16 +1,20 @@
 # Fixes for the z3attributes 'edit' object on 2.1. On 2.2 this thing
 # is in the SMILayout, so no error there.
 
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from Products.Silva.SilvaObject import Zope3ViewAttribute
 
 from silva.core.views import views as silvaviews
 from five import grok
 
-from interfaces import IPorto
 
-grok.layer(IPorto)
+grok.layer(IDefaultBrowserLayer)
+
 
 class MainLayout(silvaviews.Layout):
+    """Add a simple layout to render correctly errors in SMI/half SMI.
+    This should be only needed for Silva 2.1 using silva.core.layout.
+    """
     grok.template('mainlayout')
     grok.context(Zope3ViewAttribute)
 
