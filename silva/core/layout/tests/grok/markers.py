2371842a9c5d0df962d8ad058406f8b5fb67cb71
Sylvain Viollon <thefunny@gmail.com>
2010-03-31 09:05:38 +0000

    Fix tests.

--- silva/core/layout/tests/grok/markers.py
+++ silva/core/layout/tests/grok/markers.py
@@ -19,29 +19,31 @@
 
   We can grok our marker:
 
-    >>> grok('Products.Silva.tests.grok.markers')
+    >>> grok('silva.core.layout.tests.grok.markers')
 
   So now we should have it:
 
     >>> from silva.core.layout.interfaces import IMarkManager
-    >>> from Products.Silva.tests.grok.markers import IPhotoFolderTag
+    >>> from silva.core.layout.tests.grok.markers import IPhotoFolderTag
     >>> manager = IMarkManager(folder)
     >>> manager.availableMarkers
-    [u'Products.Silva.tests.grok.markers.IPhotoFolderTag',
-     u'silva.core.layout.interfaces.ICustomizableMarker']
+    [u'Products.Silva.Folder.IPhotoGallery',
+     u'silva.core.layout.interfaces.ICustomizableMarker',
+     u'silva.core.layout.tests.grok.markers.IPhotoFolderTag']
     >>> manager.usedMarkers
     []
     >>> IPhotoFolderTag.providedBy(folder)
     False
-    >>> manager.addMarker(u'Products.Silva.tests.grok.markers.IPhotoFolderTag')
+    >>> manager.addMarker(u'silva.core.layout.tests.grok.markers.IPhotoFolderTag')
 
   And it will be available on the object:
 
     >>> manager = IMarkManager(folder)
     >>> manager.availableMarkers
-    [u'silva.core.layout.interfaces.ICustomizableMarker']
+    [u'Products.Silva.Folder.IPhotoGallery',
+     u'silva.core.layout.interfaces.ICustomizableMarker']
     >>> manager.usedMarkers
-    ['Products.Silva.tests.grok.markers.IPhotoFolderTag']
+    ['silva.core.layout.tests.grok.markers.IPhotoFolderTag']
     >>> IPhotoFolderTag.providedBy(folder)
     True
     >>> browser.go('http://localhost/root/folder/photo')
@@ -52,14 +54,15 @@
   And we can remove it:
 
     >>> manager.removeMarker(
-    ...       u'Products.Silva.tests.grok.markers.IPhotoFolderTag')
+    ...       u'silva.core.layout.tests.grok.markers.IPhotoFolderTag')
 
   It won't exists anymore:
 
     >>> manager = IMarkManager(folder)
     >>> manager.availableMarkers
-    [u'Products.Silva.tests.grok.markers.IPhotoFolderTag',
-     u'silva.core.layout.interfaces.ICustomizableMarker']
+    [u'Products.Silva.Folder.IPhotoGallery',
+     u'silva.core.layout.interfaces.ICustomizableMarker',
+     u'silva.core.layout.tests.grok.markers.IPhotoFolderTag']
     >>> manager.usedMarkers
     []
     >>> IPhotoFolderTag.providedBy(folder)
@@ -76,7 +79,7 @@
     >>> sm = getGlobalSiteManager()
     >>> sm.unregisterUtility(
     ...         IPhotoFolderTag, ICustomizableType,
-    ...         u'Products.Silva.tests.grok.markers.IPhotoFolderTag')
+    ...         u'silva.core.layout.tests.grok.markers.IPhotoFolderTag')
     True
 
 """
