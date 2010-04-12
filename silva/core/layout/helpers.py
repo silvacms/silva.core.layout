0cbe91fda1e82676a1e96f7f52b25f137bb85c1f
Antonin Amand <antonin.amand@gmail.com>
2010-04-12 10:57:09 +0000

    import helpers form SilvaLayout and fix schema filename

---
+++ silva/core/layout/helpers.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2002-2010 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id$
+
+# Zope
+from AccessControl import ModuleSecurityInfo
+
+# Zope3
+from zope import component
+from zope.publisher.interfaces.browser import IBrowserSkinType
+
+from silva.core.layout.interfaces import ISilvaSkin
+
+# Helpers to integrate Zope3, Five and SilvaLayout functionality
+# with Zope2 and Silva
+
+__allow_access_to_unprotected_subobjects__ = 1
+
+module_security = ModuleSecurityInfo('silva.core.layout.helpers')
+
+module_security.declareProtected(
+    'Access Contents Information', 'getAvailableSkins')
+def getAvailableSkins():
+    skins = component.getUtilitiesFor(IBrowserSkinType)
+    return [name for name, skin in skins if skin.extends(ISilvaSkin)]
