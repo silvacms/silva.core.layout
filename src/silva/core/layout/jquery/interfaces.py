dfa3e4c0cd4a0051b720decd630030d2feee9ee9
Sylvain Viollon <thefunny@gmail.com>
2010-09-23 09:41:36 +0000

    Split JQuery and JQueryUI.

--- src/silva/core/layout/jquery/interfaces.py
+++ src/silva/core/layout/jquery/interfaces.py
@@ -1,9 +1,16 @@
+# Copyright (c) 2002-2010 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id$
+
 from silva.core import conf as silvaconf
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
 class IJQueryResources(IDefaultBrowserLayer):
     silvaconf.resource('jquery-1.4.2.min.js')
+
+
+class IJQueryUIResources(IJQueryResources):
     silvaconf.resource('jquery-ui-1.8.4.custom.min.js')
     silvaconf.resource('jquery-ui-i18n.js')
     silvaconf.resource('jquery-ui-1.8.4.custom.css')
