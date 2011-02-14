7af561a5f1163fb683e1543e50ff334f7eb3e085
Sylvain Viollon <thefunny@gmail.com>
2011-02-14 16:57:51 +0100

    Add JSON-template.

--- src/silva/core/layout/jquery/__init__.py
+++ src/silva/core/layout/jquery/__init__.py
@@ -1,1 +1,9 @@
-# package
+# -*- coding: utf-8 -*-
+# Copyright (c) 2010-2011 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id$
+
+from silva.core.layout.jquery.interfaces import IJQueryResources
+from silva.core.layout.jquery.interfaces import IJQueryUIResources
+
+__all__ = ['IJQueryResources', 'IJQueryUIResources']
