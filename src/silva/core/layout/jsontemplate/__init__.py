67e69d07e4f53a1eaa490480c715e9595d1a911f
Sylvain Viollon <thefunny@gmail.com>
2011-07-21 16:40:09 +0200

    Remove jsontemplate from here.

--- src/silva/core/layout/jsontemplate/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2011 Infrae. All rights reserved.
-# See also LICENSE.txt
-# $Id$
-
-from silva.core.layout.jsontemplate.interfaces import IJsonTemplateResources
-
-__all__ = ['IJsonTemplateResources']
