612394125304d02661275fdaa3624e4c11c2c470
Andy Altepeter <aaltepet@altepeter.net>
2012-03-16 12:55:25 -0500

    add jqueryuitimepicker

--- src/silva/core/layout/jquery/interfaces.py
+++ src/silva/core/layout/jquery/interfaces.py
@@ -11,6 +11,8 @@ class IJQueryResources(IDefaultBrowserLayer):
 
 class IJQueryUIResources(IJQueryResources):
     silvaconf.resource('jquery-ui-1.8.4.custom.min.js')
+    silvaconf.resource('timepicker/jquery.ui.timepicker.js')
     # including this file makes the datepicker chinese
     #silvaconf.resource('jquery-ui-i18n.js')
     silvaconf.resource('jquery-ui-1.8.4.custom.css')
+    silvaconf.resource('timepicker/jquery.ui.timepicker.css')
